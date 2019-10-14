aa7762ff964deb869e93b0bf34fb8aa926b65126
Bethany Wride <bethanywride@Bethanys-MacBook-Air-2.local>
2019-10-14 14:42:48 -0600

    Finished model controller file

---
+++ JournalCloudKit/JournalCloudKit/Controllers/Model Controllers/EntryController.swift
@@ -0,0 +1,65 @@
+//
+//  EntryController.swift
+//  JournalCloudKit
+//
+//  Created by Bethany Wride on 10/14/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class EntryController {
+    // MARK: - Global variables
+    var entries: [Entry] = []
+    static let sharedInstance = EntryController()
+    let privateCloudDatabase = CKContainer.default().privateCloudDatabase
+    
+    // MARK: - CRUD
+    func createEntry(with title: String, bodyText: String, completion: @escaping (_ success: Bool) -> Void) {
+        let newEntry = Entry(title: title, bodyText: bodyText)
+        save(entry: newEntry) { (success) in
+            if success {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    } // End of function
+    
+    func readEntries(completion: @escaping (_ success: Bool) -> Void) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: EntryConstants.recordTypeKey, predicate: predicate)
+        privateCloudDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+                return
+            }
+            guard let unwrappedRecords = records else { completion(false) ; return }
+            let entriesArray = unwrappedRecords.compactMap( {Entry(ckRecord: $0)})
+            self.entries = entriesArray
+            print("Fetched entries array successfully")
+            completion(true)
+        }
+    } // End of function
+    
+    // MARK: - Save and Load
+    func save(entry: Entry, completion: @escaping (_ success: Bool) -> Void) {
+        let newEntry = Entry(title: entry.title, bodyText: entry.bodyText)
+        let entryRecord = CKRecord(entry: newEntry)
+        privateCloudDatabase.save(entryRecord) { (record, error) in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+                return
+            }
+            guard let unwrappedRecord = record,
+                let savedEntry = Entry(ckRecord: unwrappedRecord)
+                else { completion(false) ; return }
+            self.entries.append(savedEntry)
+            print("Saved entry successfully")
+            completion(true)
+        } 
+    } // End of function
+} // End of class
---
+++ JournalCloudKit/JournalCloudKit/Models/Entry.swift
@@ -0,0 +1,56 @@
+//
+//  Entry.swift
+//  JournalCloudKit
+//
+//  Created by Bethany Wride on 10/14/19.
+//  Copyright © 2019 Bethany Wride. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+// MARK: - Constant Strings
+struct EntryConstants {
+    static let titleKey = "title"
+    static let bodyKey = "bodyText"
+    static let timestampKey = "timestamp"
+    static let recordTypeKey = "Entry"
+}
+
+// MARK: - Class Declaration
+
+class Entry {
+    let title: String
+    let bodyText: String
+    let timestamp: Date
+    let ckRecord: CKRecord.ID
+    
+    init(title: String, bodyText: String, timestamp: Date = Date(), ckRecord: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+        self.title = title
+        self.bodyText = bodyText
+        self.timestamp = timestamp
+        self.ckRecord = ckRecord
+    }
+} // End of class
+
+// MARK: - Extensions
+extension Entry {
+    convenience init?(ckRecord: CKRecord) {
+        guard let title = ckRecord[EntryConstants.titleKey] as? String,
+            let bodyText = ckRecord[EntryConstants.bodyKey] as? String,
+            let timestamp = ckRecord[EntryConstants.timestampKey] as? Date
+            else { return nil }
+        self.init(title: title, bodyText: bodyText, timestamp: timestamp, ckRecord: ckRecord.recordID)
+    }
+}
+
+extension CKRecord {
+    convenience init(entry: Entry) {
+        self.init(recordType: EntryConstants.recordTypeKey, recordID: entry.ckRecord)
+        self.setValue(entry.title, forKey: EntryConstants.titleKey)
+        self.setValue(entry.bodyText, forKey: EntryConstants.bodyKey)
+        self.setValue(entry.timestamp, forKey: EntryConstants.timestampKey)
+    }
+}
+
+
